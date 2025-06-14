bbae32690806800692ec894a09b4d29c59b113d9
emmanuel-adewunmi <eodedina@byupathway.edu>
2025-06-15 00:55:17 +0100

    Week 06 Learning activity fulfilled

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,31 @@
 using System;
 
+using System.Collections.Generic;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Create and test individual shapes
+        Square square = new Square("Red", 4);
+        Console.WriteLine($"Square - Color: {square.GetColor()}, Area: {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Blue", 5, 3);
+        Console.WriteLine($"Rectangle - Color: {rectangle.GetColor()}, Area: {rectangle.GetArea()}");
+
+        Circle circle = new Circle("Green", 2);
+        Console.WriteLine($"Circle - Color: {circle.GetColor()}, Area: {circle.GetArea()}");
+
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        Console.WriteLine("\nIterating through shapes list:");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape - Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
